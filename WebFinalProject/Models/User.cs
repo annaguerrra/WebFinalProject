c4b8e7d407545d4f267faf352a27276089323120
annaguerrra <annaguerracastro@gmail.com>
2025-08-20 13:35:42 -0300

    Login Request, not finished

--- WebFinalProject/Models/User.cs
+++ WebFinalProject/Models/User.cs
@@ -5,6 +5,7 @@ public class User
     public Guid ID { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
     public string Image { get; set; }
     public DateTime ExpirationDate { get; set; }
     public string Bio { get; set; }
