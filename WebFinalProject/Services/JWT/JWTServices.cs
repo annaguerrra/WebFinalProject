e86d3c824edcd5848db65b6de872dc313d81aad9
annaguerrra <annaguerracastro@gmail.com>
2025-08-22 15:20:00 -0300

    fkfjnitr

--- WebFinalProject/Services/JWT/JWTServices.cs
+++ WebFinalProject/Services/JWT/JWTServices.cs
@@ -4,7 +4,6 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
-
 public class JWTService : IJWTServices
 {
     public string CreateToken(UserToAuth data)
