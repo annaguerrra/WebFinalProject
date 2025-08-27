2beec1ecdff26bbd12137f13ff75309665c5cb67
annaguerrra <annaguerracastro@gmail.com>
2025-08-27 15:47:25 -0300

    guid to int

--- WebFinalProject/Models/Role.cs
+++ WebFinalProject/Models/Role.cs
@@ -2,7 +2,7 @@ namespace WebFinalProject.Models;
 
 public class Role
 {
-    public Guid ID { get; set; }
+    public int ID { get; set; }
     public string Name { get; set; }
     public ICollection<Access> Accesses { get; set; } = [];
 }
