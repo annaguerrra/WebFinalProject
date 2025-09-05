3b7abc12ec7a9d01897be4b4cc4d48dd68b774e2
taykas <thaynaschaeffer@gmail.com>
2025-09-05 16:00:30 -0300

    Starting user controllers

--- WebFinalProject/Controllers/UserController.cs
+++ WebFinalProject/Controllers/UserController.cs
@@ -1,5 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using WebFinalProject.Models;
+using WebFinalProject.UseCases;
+using WebFinalProject.UseCases.Users.CreateUser;
 
 namespace WebFinalProject.Controller;
 
-// [ApiController]
+[ApiController]
+[Route("??")]
+public class UserController : ControllerBase
+{
+    [HttpPost("??")]
+    public async Task<CreateUserResponse> CreateUser([FromBody]CreateUserRequest user)
+    {
+        var request = new CreateUserRequest 
+    }
+}
